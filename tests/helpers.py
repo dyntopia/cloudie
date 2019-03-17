d0ab93f42ac12082a8dbf22c178899ec2debfe6b
Hans Jerry Illikainen <hji@dyntopia.com>
2019-03-17 18:09:00 +0000

    tests: add TexttableMock

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,7 +1,9 @@
 import tempfile
 import unittest
+from typing import List
 
 import click.testing
+from texttable import Texttable
 
 
 class ClickTestCase(unittest.TestCase):
@@ -11,3 +13,14 @@ class ClickTestCase(unittest.TestCase):
 
     def tearDown(self) -> None:
         self.config.close()
+
+
+class TexttableMock(Texttable):  # type: ignore
+    def __init__(self, max_width: int = 80) -> None:
+        super().__init__(max_width)
+        self.headers = []  # type: List[str]
+        self.rows = []  # type: List[List[str]]
+
+    def draw(self) -> None:
+        self.headers = self._header
+        self.rows = self._rows
